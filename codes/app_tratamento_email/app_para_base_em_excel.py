55ca90a59e21365004aebfe8a7ce00d6d7c3240f
matheuslmarchetti <lunguinhomarchetti@gmail.com>
2024-03-25 05:32:32 -0300

    adição de linhas para salvar emils tratados em um novo arquivos excel

--- codes/app_tratamento_email/app_para_base_em_excel.py
+++ codes/app_tratamento_email/app_para_base_em_excel.py
@@ -5,14 +5,19 @@ from funcoes_de_apoio import validar_email as ve
 
 tabela = pd.read_excel(fr'C:\Users\lungu\Documents\Python\github\Repositorio-De-Testes-Diversos-De-Python\bases\bases_emails.xlsx')
 
+df = pd.DataFrame()
+
 for linha in tabela.index:
+    
     emails = tabela.loc[linha, "E-mail"]
     
     lista_emails_normalizados = ne.normalize_email(emails)
     lista_emails_validados = ve.valide_email(lista_emails_normalizados)
     emails_tratados = ";".join(lista_emails_validados)
 
-    print(emails)
-    print('------------------------------------------------')
-    print(emails_tratados)
-    print('------------------------------------------------')
+    if emails_tratados != "":
+        df = df._append({'Email': emails_tratados}, ignore_index=True)
+    else:
+        continue
+
+df.to_excel(fr'C:\Users\lungu\Documents\Python\github\Repositorio-De-Testes-Diversos-De-Python\bases\emails_tratados.xlsx', sheet_name='e-mail', header=True, index=False)
